10a6d2221be438e5d9cfb47f1ace27cb492d6c67
dice-game <darya28.sk@gmail.com>
2025-07-13 18:30:18 +0530

    Fix route conflict by reordering message routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express, { Router } from "express"
-import {checkAuth,login,logout,signup,updateProfile} from "../contollers/auth.controller.js"
+import {checkAuth,login,logout,signup,updateProfile} from "../controllers/auth.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.post("/signup",signup)
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSideBar ,getMessages,sendMessages} from "../contollers/message.controller.js";
+import { getUsersForSideBar ,getMessages,sendMessages} from "../controllers/message.controller.js";
 const router = express.Router();
 router.get("/users",protectRoute, getUsersForSideBar);
 router.post("/send/:id",protectRoute,sendMessages);
