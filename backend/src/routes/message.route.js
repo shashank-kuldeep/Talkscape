6c44ccae78494a8cf936d7f362227df06335a914
dice-game <darya28.sk@gmail.com>
2025-07-13 18:19:54 +0530

    Fix route conflict by reordering message routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,6 +3,6 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getUsersForSideBar ,getMessages,sendMessages} from "../contollers/message.controller.js";
 const router = express.Router();
 router.get("/users",protectRoute, getUsersForSideBar);
-router.get("/:id",protectRoute,getMessages);
 router.post("/send/:id",protectRoute,sendMessages);
+router.get("/:id",protectRoute,getMessages);
 export default router
